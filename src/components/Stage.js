0718663189f91b449b4c5d0fae7bf6ccc7b5f410
“Gorgutzz” <leo.kalshteyn@gmail.com>
2022-01-15 15:01:40 -0800

    setup terominos

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,7 +3,10 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
-  <div><Cell /></div>
-)
+  <div>
+    (stage.map(now => row.map((cell, x) => <cell key={x} type={cell[0]} />))}
+
+  </div>
+);
 
 export default Stage;
