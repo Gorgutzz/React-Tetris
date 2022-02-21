1e6b116aba72a1ebe79fb4ead64ed78d6a87fdb1
“Gorgutzz” <leo.kalshteyn@gmail.com>
2022-02-20 19:52:24 -0800

    usePlayer edits

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
 import { useState, useCallback } from 'react';
 
-import { randomTetromino } from '../tetrominos';
+import { TETROMINOS, randomTetromino } from '../tetrominos';
+import { STAGE_WIDTH } from '../gameHelpers';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
