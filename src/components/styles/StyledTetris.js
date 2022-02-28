ee4018401242d49928cae05de8189dc66a8a3d3f
“Gorgutzz” <leo.kalshteyn@gmail.com>
2022-02-27 22:32:18 -0800

    style and font fix

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+// BG Image
 import bgImage from '../../img/bg.png';
 
 export const StyledTetrisWrapper = styled.div`
@@ -16,6 +16,7 @@ export const StyledTetris = styled.div`
   padding: 40px;
   margin: 0 auto;
   max-width: 900px;
+
   aside {
     width: 100%;
     max-width: 200px;
